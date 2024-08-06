c8773f3e030b07433215bac26f1b620e89596f20
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-06 21:26:41 +0300

    update amenities endpoints

--- models/amenity.py
+++ models/amenity.py
@@ -1,75 +1,34 @@
 #!/usr/bin/python3
-"""amenities"""
-from api.v1.views import app_views
-from flask import jsonify, abort, request
-from models import storage
-from models.amenity import Amenity
-from datetime import datetime
-import uuid
+"""
+This module defines the Amenity class, which inherits
+from the BaseModel class.
+"""
+import os
+
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+from models.base_model import BaseModel, Base
+
+STORAGE_TYPE = os.getenv('HBNB_TYPE_STORAGE')
+
+parent_classes = (
+    BaseModel,
+    Base if STORAGE_TYPE == "db" else object
+)
 
 
-@app_views.route('/amenities/', methods=['GET'])
-def list_amenities():
-    '''Retrieves a list of all Amenity objects'''
-    list_amenities = [obj.to_dict() for obj in storage.all("Amenity").values()]
-    return jsonify(list_amenities)
+class Amenity(*parent_classes):
+    """
+    Amenity class represents amenities available in a place.
+    """
 
+    if STORAGE_TYPE == "db":
+        __tablename__ = 'amenities'
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'])
-def get_amenity(amenity_id):
-    '''Retrieves an Amenity object'''
-    all_amenities = storage.all("Amenity").values()
-    amenity_obj = [obj.to_dict() for obj in all_amenities
-                   if obj.id == amenity_id]
-    if amenity_obj == []:
-        abort(404)
-    return jsonify(amenity_obj[0])
-
-
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
-def delete_amenity(amenity_id):
-    '''Deletes an Amenity object'''
-    all_amenities = storage.all("Amenity").values()
-    amenity_obj = [obj.to_dict() for obj in all_amenities
-                   if obj.id == amenity_id]
-    if amenity_obj == []:
-        abort(404)
-    amenity_obj.remove(amenity_obj[0])
-    for obj in all_amenities:
-        if obj.id == amenity_id:
-            storage.delete(obj)
-            storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/amenities/', methods=['POST'])
-def create_amenity():
-    '''Creates an Amenity'''
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    if 'name' not in request.get_json():
-        abort(400, 'Missing name')
-    amenities = []
-    new_amenity = Amenity(name=request.json['name'])
-    storage.new(new_amenity)
-    storage.save()
-    amenities.append(new_amenity.to_dict())
-    return jsonify(amenities[0]), 201
-
-
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
-def updates_amenity(amenity_id):
-    '''Updates an Amenity object'''
-    all_amenities = storage.all("Amenity").values()
-    amenity_obj = [obj.to_dict() for obj in all_amenities
-                   if obj.id == amenity_id]
-    if amenity_obj == []:
-        abort(404)
-    if not request.get_json():
-        abort(400, 'Not a JSON')
-    amenity_obj[0]['name'] = request.json['name']
-    for obj in all_amenities:
-        if obj.id == amenity_id:
-            obj.name = request.json['name']
-    storage.save()
-    return jsonify(amenity_obj[0]), 200
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place',
+                                       secondary='place_amenity',
+                                       back_populates='amenities')
+    else:
+        name: str = ""
