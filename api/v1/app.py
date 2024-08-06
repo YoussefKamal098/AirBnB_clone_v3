9449b221586e8b2b1648d9688ea243d18788902f
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-06 10:32:06 +0300

    make api files executable

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 """ Main file for the API """
 import os
-
 from flask import Flask
-
 from models import storage
-
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
 
