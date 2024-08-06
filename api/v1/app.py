416a4ce01d1aeb344a13fabd1f5248c6102862f6
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-06 10:24:33 +0300

    Updated Flask application with API v1 blueprint structure

---
+++ api/v1/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Main file for the API """
+import os
+
+from flask import Flask
+
+from models import storage
+
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Close the storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    HOST = os.getenv('HBNB_API_HOST')
+    PORT = os.getenv('HBNB_API_PORT')
+
+    app.run(
+        host=(HOST if HOST else "0.0.0.0"),
+        port=(PORT if PORT else 5000),
+        threaded=True
+    )
---
+++ api/v1/views/index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Index file for the API """
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """ routes to status page """
+    return jsonify({'status': 'OK'})
