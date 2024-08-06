7af886d0da6e51dddaf01f71ad92eb4fb389a8e1
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-06 20:57:17 +0300

    fix post rule

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -93,7 +93,7 @@ def delete_city(city_id):
     return jsonify({}), 200
 
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"])
+@app_views.route("/states/<state_id>/cities/", methods=["POST"])
 def post_city(state_id):
     """Create a new City object"""
     state = storage.get(State, state_id)
