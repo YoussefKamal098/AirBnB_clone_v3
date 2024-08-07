1a164b00e51b2663b6c990db6e7ae984e49b5cc3
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-07 19:06:44 +0300

    places endpoints

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -13,7 +13,10 @@ from api.v1.views import app_views
 
 @app_views.route("/api/v1/cities/<city_id>/places")
 def get_places(city_id):
-    """Return a JSON list of all Place objects in a City"""
+    """
+    Return a JSON list of all Place objects in a City,
+    or a 404 error if the City does not exist.
+    """
     city = storage.get(City, city_id)
 
     if not city:
