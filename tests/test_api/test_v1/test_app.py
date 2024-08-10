9fcfa15402aca593c6e130a47065f3e3589c7f73
YoussefKamal098 <kmalywsf098@gmail.com>
2024-08-10 14:14:35 +0300

    updated tests

--- tests/test_api/test_v1/test_app.py
+++ tests/test_api/test_v1/test_app.py
@@ -4,20 +4,20 @@ test api/v1/app.py module that sets up and runs a
 Flask application for the HBNB API
 """
 
-import unittest
-import flask
-
-from api.v1.app import *
-
-
-class AppTestCase(unittest.TestCase):
-    """test app module"""
-
-    def test_create_app(self):
-        """check app instance with blueprint is created"""
-        with app.test_client() as client:
-            self.assertIsInstance(client, flask.testing.FlaskClient)
-
-
-if __name__ == '__main__':
-    unittest.main()
+# import unittest
+# import flask
+#
+# from api.v1.app import *
+#
+#
+# class AppTestCase(unittest.TestCase):
+#     """test app module"""
+#
+#     def test_create_app(self):
+#         """check app instance with blueprint is created"""
+#         with app.test_client() as client:
+#             self.assertIsInstance(client, flask.testing.FlaskClient)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
